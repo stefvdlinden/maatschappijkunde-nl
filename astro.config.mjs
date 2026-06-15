92ec7d81636e64f97f61d4c6f52f64357d2486d0
Stef van der Linden <stef.vd.linden@uitgeverijneo.nl>
2026-06-15 20:58:26 +0200

    Use www canonical site for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://maatschappijkunde.nl',
+  site: 'https://www.maatschappijkunde.nl',
   output: 'static',
   trailingSlash: 'always',
   integrations: [sitemap()]
