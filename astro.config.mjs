cb762014724d0374315e23c6f71bd4eb615f7989
Stef van der Linden <stef.vd.linden@uitgeverijneo.nl>
2026-06-15 21:32:58 +0200

    Set canonical site to apex domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.maatschappijkunde.nl',
+  site: 'https://maatschappijkunde.nl',
   output: 'static',
   trailingSlash: 'always',
   integrations: [sitemap()]
